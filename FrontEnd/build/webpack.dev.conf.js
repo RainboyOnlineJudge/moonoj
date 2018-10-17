a96d1419714e5019c2a03f393cdf8758b73018b0
rainboylvx <rainboylvx@qq.com>
2018-10-17 18:34:52 +0800

    fix devServer一个bug

--- FrontEnd/build/webpack.dev.conf.js
+++ FrontEnd/build/webpack.dev.conf.js
@@ -26,7 +26,7 @@ module.exports = merge(baseWebpackConfig, {
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'iandex.html',
+      filename: 'index.html',
       template: 'FrontEnd/index.html',
       inject: true
     }),
